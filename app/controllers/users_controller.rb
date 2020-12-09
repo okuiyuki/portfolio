2c3053d7ef4cd1c156bc460e7c404a7701ce3d47
okuiyuki <okui0909@gmail.com>
2020-12-09 20:30:28 +0900

    add remember me

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        
         if @user.save
+            log_in @user
+            flash[:seccess] = 'Welcome to アプリ名' 
             redirect_to @user
         else
             render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,36 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: VALID_EMAIL_REGEX}
     validates :password, length: { minimum: 6 }
 has_secure_password
+
+    #ランダムな文字列作
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    #渡された文字列をハッシュ化
+    def User.digest(string)
+        BCrypt::Password.create(string)
+    end
+
+    #ハッシュ化した文字列をDBに保存
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    #渡された文字列がremember_digestと一致するか？
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    #remember_digestをDBから消す
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
