ff75133cd4cf565f8ac684120ba417821537a158
okuiyuki <okui0909@gmail.com>
2020-12-10 20:00:17 +0900

    complete post destroy structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,25 @@ class PostsController < ApplicationController
   def new
     @post = current_user.posts.build
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = 'new post created'
+      redirect_to current_user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @post = current_user.posts.find_by(id: params[:id])
+    @post.destroy
+    redirect_to request.referrer || root_url
+  end
+
+
+  def post_params
+    params.require(:post).permit(:title, :discription, :github_url, :app_url)
+  end
 end
