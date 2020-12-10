93983789fa65c006f88ac317dbff54936f456171
okuiyuki <okui0909@gmail.com>
2020-12-10 19:25:07 +0900

    generate posts model & add post form

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+  resources :posts, only: [:new, :create, :destroy]
   
 end
