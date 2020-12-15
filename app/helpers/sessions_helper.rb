4333a7f882c050d467b2d0a5f59ad9115c6adba4
okuiyuki <okui0909@gmail.com>
2020-12-15 17:08:10 +0900

    change flash language into ja

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,7 +47,7 @@ module SessionsHelper
     def logged_in_user
         unless logged_in?
             store_location
-            flash[:danger] = 'Please log in'
+            flash[:danger] = 'ログインしてください'
             redirect_to login_url
         end
     end
