7aff362d303f97874c42d2ee7abae7ccd6afe87b
okuiyuki <okui0909@gmail.com>
2020-12-20 17:36:44 +0900

    add delete link to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,18 @@ class CommentsController < ApplicationController
         end
     end
 
+
+    def destroy
+        @comment = current_user.comments.find(params[:post_id])
+        if @comment.destroy
+            flash[:success]  = "コメントを削除しました"
+            redirect_to post_path(@comment.post.id)
+        else
+            flash[:danger] = "コメントを削除できませんでした"
+            redirect_to  post_path(@comment.post.id)
+        end
+    end
+
     def comment_params
         params.require(:comment).permit(:body, :post_id)
     end
