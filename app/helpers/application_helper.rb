6a38198a2cc3f6a97095a6dcaa684a45786bdeee
okuiyuki <okui0909@gmail.com>
2020-12-28 19:44:24 +0900

    ogp not complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,18 @@
 module ApplicationHelper
 
     def get_twitter_card_info(post)
-        post_image = post.images[0]
-        twitter_card = []
+        twitter_card = {}
         if post
-            twitter_card[:url] = "http://localhost:3000/posts/#{post.id}"
+            twitter_card[:url] = "localhost:3000/posts/#{post.id}"
             twitter_card[:title] = "おすすめ"
             twitter_card[:discription] ="タイトル #{post.title}"
+            post_image = post.images[0]
             twitter_card[:image] = post_image
         else
-            twitter_card[:url]
-            twitter_card[:title]
-            twitter_card[:discription]
-            twitter_card[:image]
+            twitter_card[:url] = "localhost:3000"
+            twitter_card[:title]  = "アプリ"
+            twitter_card[:discription] = "タイトル"
+            twitter_card[:image] = "https://webdesign-trends.net/wp/wp-content/uploads/2020/07/free-illustrations-2020.png"
         end
         twitter_card[:card] = "summary"
         twitter_card[:site] = @ZerygDVYc7IjvaU
