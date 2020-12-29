3e7e5511852df9fbbd38366b1676e4df18a336cc
okuiyuki <okui0909@gmail.com>
2020-12-29 16:16:43 +0900

    ogp set

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,22 @@ module ApplicationHelper
             post_image = post.images[0]
             twitter_card[:image] = post_image
         else
-            twitter_card[:url] = "localhost:3000"
+            twitter_card[:url] = "http://localhost:3000"
             twitter_card[:title]  = "アプリ"
             twitter_card[:discription] = "タイトル"
-            twitter_card[:image] = "https://webdesign-trends.net/wp/wp-content/uploads/2020/07/free-illustrations-2020.png"
+            twitter_card[:image] = "http://localhost:3000/assets/images/user_default.png"
         end
-        twitter_card[:card] = "summary"
+        twitter_card[:card] = "summary_large_image"
         twitter_card[:site] = @ZerygDVYc7IjvaU
+        twitter_card
     end
+
+    # <% twitter_card = get_twitter_card_info(@post ? @post : nil) %>
+    # <meta name="twitter:card" content=<%= twitter_card[:card] %> />
+    # <meta name="twitter:site" content=<%= twitter_card[:site] %> />
+    # <meta property="og:url" content=<%= twitter_card[:url] %> />
+    # <meta property="og:title" content=<%= twitter_card[:title] %> />
+    # <meta property="og:discription" content=<%= twitter_card[:discription] %> />
+    # <meta property="og:image" content=<%= twitter_card[:image]%> />
+    # <meta name="viewport" conten="width=device-width , initial-scale=1" />
 end
