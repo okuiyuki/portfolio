467a72d92cf3915c14edb4355523413740987f1f
okuiyuki <okui0909@gmail.com>
2021-01-04 22:27:31 +0900

    like function ajax complete

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,13 @@
 class LikesController < ApplicationController
 
     def create
-        @like = current_user.likes.build(post_id: params[:post_id])
-        if @like.save
-            redirect_to post_path(@like.post_id)
-        else
-            redirect_to root_path
-        end
+        @like = current_user.likes.create(post_id: params[:post_id])
+        @post = Post.find(params[:post_id])
     end
 
     def destroy
         @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
         @like.destroy
-        redirect_to post_path(@like.post_id)
+        @post = Post.find(params[:post_id])
     end
 end
