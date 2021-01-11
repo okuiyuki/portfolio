283b4b764cb82c30d1486d5127403d1b8b3f3b0e
okuiyuki <okui0909@gmail.com>
2021-01-11 15:25:38 +0900

    spec comment and category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  
+	before do
+		@category = Category.new(
+			id: 1,
+			name: "フロントエンド"
+		)
+	end
+
+	it "bodyがあれば有効" do
+		expect(@category).to be_valid
+	end
+
+	
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  
+    before do
+        @user = FactoryBot.create(:user, id:1)
+        Category.create(id: 1, name: "フロントエンド")
+        @post = @user.posts.create(
+            id: 1,
+            title: "test",
+            discription: "testdiscription",
+            user_id: 1,
+            category_id: 1
+        )
+        @comment = Comment.new(
+            body: "いいね！",
+            user_id: @user.id,
+            post_id: @post.id
+        )
+    end
+
+
+    it "body, user_id, post_idがあれば有効" do
+        expect(@comment).to be_valid
+    end
+
+    it "bodyが無ければ無効" do 
+        @comment.body = ""
+        expect(@comment).to be_invalid
+    end
+
+    it "user_idが無ければ無効" do
+        @comment.user_id = nil
+        expect(@comment).to be_invalid
+    end
+
+    it "post_idが無ければ無効" do
+        @comment.post_id = nil
+        expect(@comment).to be_invalid
+    end
+
+    it "bodyが255以上であれば無効" do 
+        @comment.body = "a" * 256
+        expect(@comment).to be_invalid
+    end
 end
