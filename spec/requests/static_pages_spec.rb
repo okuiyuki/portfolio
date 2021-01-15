fc0c7f012415e0a8f44b54e30637cf66ff3df053
okuiyuki <okui0909@gmail.com>
2021-01-15 20:08:52 +0900

    write static_page controller rspec

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe "GET /" do
+    it "successを返す" do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it "リクエストが200,OKを返す" do
+      get root_path
+      expect(response.status).to eq 200
+    end
+  end
+
+  desicribe 'GET /top_next' do
+    it 'successを返す' do
+      get top_next_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'リクエストが200,OKを返す' do
+      get top_next_path
+      expect(response.status).to eq 200
+    end
+  end
+end
