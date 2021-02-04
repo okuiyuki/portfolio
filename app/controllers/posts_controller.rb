559d2e6a97a0d334738712fa0df3cda2dd888548
okuiyuki <okui0909@gmail.com>
2021-02-05 02:00:51 +0900

    change range before action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create, :destroy]
+  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
   #before_action :correct_user, only: [:edit, :update]
 
   def new
